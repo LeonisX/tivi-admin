a9b3681128ce6c761b3803ba6fb79987b2c7c4f0
Leonis <tv-games@mail.ru>
2016-08-17 17:57:52 +0300

    Experiments JDBC, MYSQL

--- src/test/scala-2.11/md/leonis/tivi/admin/Query.scala
+++ src/test/scala-2.11/md/leonis/tivi/admin/Query.scala
@@ -1,111 +1,103 @@
 package md.leonis.tivi.admin
 
-import javafx.scene.canvas.{Canvas, GraphicsContext}
+import javafx.application.Platform
 import javafx.scene.paint.Color
 
 import md.leonis.tivi.admin.JdbcUtils._
+
 import scala.util.control.Breaks._
 import scala.collection.mutable.ListBuffer
 
-class DS {
+class MySqlProcess {
+  var running: Boolean = true
+  var threshold: Int = 100
+  var count: Int = 1
+  var cache: ListBuffer[(Long, Double)] = ListBuffer[(Long, Double)]()
+  withCount(count)
   var prev: (Long, Double) = (-1, 0.0)
   var next: (Long, Double) = (-1, 0.0)
 
-  def add(index: Long, value: Double): DS = {
-    prev = next
-    next = index -> value
+  var function = () => 0.0
+
+  def process(index: Long): MySqlProcess = {
+    if (index % threshold == 0) {
+      cache.clear()
+      (1 to count).foreach(i => cache.append(index -> function()))
+      prev = next
+      var result = average
+      val diff = result - prev._2
+      if (Math.abs(diff) > prev._2 / 10) result = prev._2 + diff / 5
+      next = index -> result
+    }
     this
   }
+
+  def withFunction(function: () => Double): MySqlProcess = {
+    this.function = function
+    this
+  }
+
+  def withThreshold(threshold: Int): MySqlProcess = {
+    this.threshold = threshold
+    this
+  }
+
+  def withCount(count: Int): MySqlProcess = {
+    this.count = count
+    this
+  }
+
+  def stop(): Unit = {
+    running = false
+  }
+
+  def average = {
+    cache.map(c => c._2).sum / count
+  }
+}
+
+object MySqlProcess {
+  val scale: Int = 1000
+  def apply() = new MySqlProcess
 }
 
 object Query {
   val r = scala.util.Random
-  val smallSize = 1000
-  val smallList = ListBuffer[Double]()
-  val bigList = ListBuffer[Double]()
+  //val smallSize = 1000
+  //val smallList = ListBuffer[Double]()
+  //val bigList = ListBuffer[Double]()
 
-  val in = new DS
-  val i2 = new DS
-  val i3 = new DS
-  val i4 = new DS
+  val in = MySqlProcess().withFunction(insertToVideo).withThreshold(1)
+  val i2 = MySqlProcess().withFunction(selectFromVideo).withThreshold(1000).withCount(1)
+  val i3 = MySqlProcess().withFunction(selectFromVideo2).withThreshold(1000).withCount(1)
+  val i4 = MySqlProcess().withFunction(selectFromVideo3).withThreshold(1000).withCount(1)
 
   def go(start: Start) {
 
     executeUpdate("TRUNCATE video;")
     executeUpdate("TRUNCATE video_tag;")
 
-
-
     breakable {
       for (i <- 0 until 10000000) {
         if (!start.running) break
-        smallList += insertToVideo
-        if (smallList.size >= smallSize) {
-          val avg = average(smallList)
-          bigList += avg
-          smallList.clear()
-          /*println(f"${i + 1}%10d: $avg%1.5f")
-        println(f"in${i + 1}%8d: $selectFromVideo%1.5f")
-        println(f"i2${i + 1}%8d: $selectFromVideo2%1.5f")
-        println(f"i3${i + 1}%8d: $selectFromVideo3%1.5f")*/
-          in.add(i / smallSize, avg)
-          i2.add(i / smallSize, selectFromVideo)
-          i3.add(i / smallSize, selectFromVideo2)
-          i4.add(i / smallSize, selectFromVideo3)
-          /*start.drawShapes(in, Color.GRAY)
-        start.drawShapes(i2, Color.GRAY)
-        start.drawShapes(i3, Color.GRAY)
-        start.drawShapes(i4, Color.GRAY)*/
-          start.drawShapes(in, Color.BLACK)
-          start.drawShapes(i2, Color.BLUE)
-          start.drawShapes(i3, Color.GREEN)
-          start.drawShapes(i4, Color.RED)
-        }
+          in.process(i)
+         i2.process(i)
+          i3.process(i)
+          i4.process(i)
+
+            Platform.runLater(new Runnable() {
+              def run() {
+                start.drawShapes(in, Color.BLACK)
+                start.drawShapes(i2, Color.BLUE)
+                start.drawShapes(i3, Color.GREEN)
+                start.drawShapes(i4, Color.RED)
+              }
+            })
+
+
+        //start.canvas.getGraphicsContext2D.save()
       }
     }
-/*
-    def drawShapes(ds: DS, color: Color) {
-      val gc = canvas.getGraphicsContext2D
-      gc.setStroke(color)
-      gc.setLineWidth(0.5)
-      gc.strokeLine(ds.prev._1, 800 - ds.prev._2 * 1000, ds.next._1, 800 - ds.next._2 * 1000)
-    }*/
-
-    def insertToVideo(): Double = {
-      val start = System.nanoTime()
-      val preparedStatement = getConnection.prepareStatement(
-        "INSERT INTO video (title, locurl, exturl, descript, keywords, textshort, textmore, textnotice, mirrorsname, mirrorsurl, tags) " +
-          "VALUES (?, '', '', '', '', '', '', '', '', '', ?)")
-      preparedStatement.setString(1, "title " + randomString(7))
-      preparedStatement.setString(2, "tag" + r.nextInt(100))
-      //(0 until r.nextInt(10)).map("tag" + r.nextInt(100)).mkString(",")
-      preparedStatement.executeUpdate()
-      (System.nanoTime() - start) / 1000000000.0
-    }
-
-    def selectFromVideo: Double = {
-      val start = System.nanoTime()
-      val rs = executeQuery("SELECT * FROM video WHERE tags = '" + "tag" + r.nextInt(100) + "'")
-      (System.nanoTime() - start) / 1000000000.0
-    }
-
-    def selectFromVideo2: Double = {
-      val start = System.nanoTime()
-      val rs = executeQuery("SELECT * FROM video WHERE tags LIKE '" + "tag" + r.nextInt(100) + "%'")
-      (System.nanoTime() - start) / 1000000000.0
-    }
-
-    def selectFromVideo3: Double = {
-      val start = System.nanoTime()
-      val rs = executeQuery("SELECT * FROM video WHERE tags LIKE '%" + "tag" + r.nextInt(100) + "%'")
-      (System.nanoTime() - start) / 1000000000.0
-    }
-
-    def randomString(len: Int): String = {
-      (1 to len).map(i => r.nextPrintableChar).mkString
-    }
-
-    //bigList.foreach(time => println(f"ns: $time%1.5f"))
 
     for (i <- 1 to 100) {
       val preparedStatement2 = getConnection.prepareStatement(
@@ -133,6 +125,40 @@ object Query {
     /*def nonEmpty(str: String): Boolean = {
       str.nonEmpty && str != "0"
     }*/
+  }
+
+  def randomString(len: Int): String = {
+    (1 to len).map(i => r.nextPrintableChar).mkString
+  }
+
+  def insertToVideo() = {
+    val start = System.nanoTime()
+    val preparedStatement = getConnection.prepareStatement(
+      "INSERT INTO video (title, locurl, exturl, descript, keywords, textshort, textmore, textnotice, mirrorsname, mirrorsurl, tags) " +
+        "VALUES (?, '', '', '', '', '', '', '', '', '', ?)")
+    preparedStatement.setString(1, "title " + randomString(7))
+    preparedStatement.setString(2, "tag" + r.nextInt(100))
+    //(0 until r.nextInt(10)).map("tag" + r.nextInt(100)).mkString(",")
+    preparedStatement.executeUpdate()
+    (System.nanoTime() - start) / 1000000000.0
+  }
+
+  def selectFromVideo() = {
+    val start = System.nanoTime()
+    val rs = executeQuery("SELECT * FROM video WHERE tags = '" + "tag" + r.nextInt(100) + "'")
+    (System.nanoTime() - start) / 1000000000.0
+  }
+
+  def selectFromVideo2() = {
+    val start = System.nanoTime()
+    val rs = executeQuery("SELECT * FROM video WHERE tags LIKE '" + "tag" + r.nextInt(100) + "%'")
+    (System.nanoTime() - start) / 1000000000.0
+  }
+
+  def selectFromVideo3() = {
+    val start = System.nanoTime()
+    val rs = executeQuery("SELECT * FROM video WHERE tags LIKE '%" + "tag" + r.nextInt(100) + "%'")
+    (System.nanoTime() - start) / 1000000000.0
   }
 
   def average[T]( ts: Iterable[T] )( implicit num: Numeric[T] ) = {
--- src/test/scala-2.11/md/leonis/tivi/admin/Start.scala
+++ src/test/scala-2.11/md/leonis/tivi/admin/Start.scala
@@ -1,11 +1,11 @@
 package md.leonis.tivi.admin
 
+import java.util.{Timer, TimerTask}
 import javafx.application.Application
 import javafx.event.EventHandler
 import javafx.scene.Group
 import javafx.scene.Scene
 import javafx.scene.canvas.Canvas
-import javafx.scene.canvas.GraphicsContext
 import javafx.scene.paint.Color
 import javafx.stage.{Stage, WindowEvent}
 
@@ -17,7 +17,7 @@ object Start {
 }
 
 class Start extends Application {
-  val canvas = new Canvas(1200, 600)
+  val canvas = new Canvas(1600, 600)
   var running = true
 
   override def start(primaryStage: Stage) {
@@ -44,13 +44,21 @@ class Start extends Application {
 
   }
 
-  def drawShapes(ds: DS, color: Color) {
+  def drawShapes(process: MySqlProcess, color: Color) {
     val gc = canvas.getGraphicsContext2D
-    gc.setStroke(color)
+
     gc.setLineWidth(0.5)
+    gc.setStroke(Color.GRAY)
+    gc.strokeLine(50.5, canvas.getHeight - 50.5, canvas.getWidth, canvas.getHeight - 50.5)
+    gc.strokeLine(50.5, canvas.getHeight - 50.5, 50.5, 0)
+
+    gc.setStroke(color)
+    gc.translate(50.5,  - 50.5)
     //gc.getPixelWriter.setColor(ds.prev._1.toInt, (canvas.getHeight - 50 - ds.prev._2 * 1000).toInt, color)
-    gc.strokeLine(ds.prev._1, canvas.getHeight - 50 - ds.prev._2 * 1000, ds.next._1, canvas.getHeight - 50 - ds.next._2 * 1000)
+    val m = process.threshold * ((1001 - process.threshold) + 0.000000000000000001)
+    gc.strokeLine(process.prev._1 / m, canvas.getHeight - process.prev._2 * 500, process.next._1 / m, canvas.getHeight - process.next._2 * 500)
+    gc.translate(-50.5, 50.5)
     gc.clearRect(0,0,300,100)
-    gc.fillText((ds.prev._1 * 1000).toString, 50, 50)
+    gc.fillText(process.prev._1.toString, 50, 50)
   }
 }
